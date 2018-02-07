d6190146e0104c049fe8e27469ef3bca3593bfbb
kimgahyung <kimgahyung@gmail.com>
2018-02-06 21:03:54 -0700

    updated constraint

--- src/Constraint.java
+++ src/Constraint.java
@@ -1,5 +1,4 @@
 import java.util.ArrayList;
-import java.util.Arrays;
 
 /**
  * 
@@ -9,101 +8,86 @@ import java.util.Arrays;
 
 public class Constraint {
 	
-    public final static int FORCED_PARTIAL_ASSIGNMENT = 0;
-    public final static int FORBIDDEN_MACHINE = 1;
-    public final static int TOO_NEAR_TASKS = 2;
-	public final static int MACHINE_PENALTIES = 3;
-    public final static int TOO_NEAR_PENALTIES = 4;
+    private ArrayList<char[]> forced; 
+    private ArrayList<char[]> forbidden; 
+    private ArrayList<char[]> tooNearTasks;
+    private ArrayList<char[]> tooNearPenalties; 
+    private int[][] penalties;
     
-    private int type;
+    private Data data;
     
-    private ArrayList<Object> forced; // {Integer, Character, Integer, Character ...}
-    private ArrayList<Object> forbidden; 
-    private ArrayList<Character> tooNearTasks;
-    private int[][] machinePenalties;
-    private ArrayList<Object> tooNearPenalties; // {Character, Character, Integer, Character, Character, Integer ...}
-  
-    
-    public Constraint(int constraintType) {
-		this.type = constraintType;
-		switch (constraintType) {
-		case FORCED_PARTIAL_ASSIGNMENT:
-			this.forced = new ArrayList<>();
-			break;
-		case FORBIDDEN_MACHINE:
-			this.forbidden = new ArrayList<>();
-			break;
-		case TOO_NEAR_TASKS:
-			this.tooNearTasks = new ArrayList<>();
-			break;
-		case MACHINE_PENALTIES:
-			this.machinePenalties = new int[8][8];
-			break;
-		default:
-			this.tooNearPenalties = new ArrayList<>();
-			break;
-		}
+    public Constraint() {
+    	
+    	// Get data from the class Data
+    	this.data = new Data();
+    	this.forced = data.getForced();
+    	this.forbidden = data.getForbidden();
+    	this.tooNearTasks = data.getTooNearTasks();
+    	this.tooNearPenalties = data.getTooNearPenalties();
+    	this.penalties = data.getPenalties();
+    	
+    	setPenaltySet();
+		
 	}
     
-    public boolean isForced(int machine, char task) {
-    	Integer intObj = new Integer(machine);
-    	Character charObj = new Character(task);
-    	for (int i=0; i<forced.size(); i++) {
-    		if (forced.get(i) instanceof Integer) {
-    			if (intObj.equals((Integer)forced.get(i)) && charObj.equals((Character)forced.get(i+1))) {
-    				return true;
-    			}
+    private void setPenaltySet() {
+    	
+    	setForcedConstraints();
+    	setForbiddenConstraints();
+    	
+    }
+    
+    /**
+     * This method applies forced constraints to the penalty array.
+     * If there is forced constraint (machine m, task t), 
+     * Only element in index (m,t) of penalty array is valid. 
+     * ( ,t) and (m, ) are set to null.
+     */
+    private void setForcedConstraints() {
+    	
+    	for (char[] c : forced) {
+    		
+    		int machine = Character.getNumericValue(c[0]) - 1;
+    		int task = c[1] - 65;
+    		
+    		// Set elements in (machine, ) to null
+    		for (int i = 0; i < penalties[machine].length; i++) {
+    			penalties[machine][i] = null;
     		}
+    		
+    		// Set elements in ( , task) to null
+    		for (int i = 0; i < penalties.length; i++) {
+    			penalties[i][task] = null;
+    		}
+    	}	
+    }
+    
+    /**
+     * This method applies forbidden constraints to the penalty array.
+     * If there is forbidden constraint (machine m, task t), 
+     * an element in index (m,t) is set to null.
+     */
+    private void setForbiddenConstraints() {
+    	
+    	for (char[] c : forbidden) {
+    		
+    		int machine = Character.getNumericValue(c[0]) - 1;
+    		int task = c[1] - 65;
+    		
+    		penalties[machine][task] = null;
     	}
-    	return false;
     }
     
-    public void setForcedConstraints(ArrayList<Object> forcedConstraints) {
-    	forced = forcedConstraints;
-    }
-    
-    public ArrayList<Object> getForcedConstraints() {
-    	return new ArrayList<>(forced);
-    }
-    
-    public void setForbiddenConstraints(ArrayList<Object> forbiddenConstraints) {
-    	forbidden = forbiddenConstraints;
-    }
-    
-    public ArrayList<Object> getForbiddenConstraints() {
-    	return  new ArrayList<>(forbidden);
-    }
-    
-    public void setTooNearTasks(ArrayList<Character> tooNearTasks) {
-    	this.tooNearTasks = tooNearTasks;
-    }
-    
-    public ArrayList<Character> getTooNearTasks() {
+    public ArrayList<char[]> getTooNearTasks() {
     	return new ArrayList<>(tooNearTasks);
     }
-
-    public void setMachinePenalties(int[][] ar) {
-    	machinePenalties = ar;
-    }
     
-    public int[][] getMachinePenalties() {
-    	return machinePenalties;
-    }
-    
-    public void setTooNearPenalties(ArrayList<Object> tooNearPenalties) {
-    	this.tooNearPenalties = tooNearPenalties;
-    }
-    
-    public ArrayList<Object> getTooNearPenalties() {
+    public ArrayList<char[]> getTooNearPenalties() {
     	return new ArrayList<>(tooNearPenalties);
     }
     
-	public int getType() {
-		return type;
-	}
-
-	public void setType(int type) {
-		this.type = type;
-	}
+    public int[][] getPenalties() {
+    	return penalties;
+    }
     
 }
