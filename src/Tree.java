d348cf481493e13ff88667e71d80cca8551546c1
Christilyn A <christilynarjona@outlook.com>
2018-02-07 21:10:16 -0700

    Fixed errors on Tree.java

--- src/Tree.java
+++ src/Tree.java
@@ -1,9 +1,9 @@
 import java.util.ArrayList;
 
 public class Tree {
-		Constraint constraint;
-		Node rootNode;
-		ArrayList<Character> finalSol;
+		static Constraint constraint;
+		static Node rootNode;
+		static ArrayList<Character> finalSol;
 		static int currentLowerBound;
 		
 	public Tree() {
@@ -15,11 +15,11 @@ public class Tree {
 	public static void main(String[] args) {
 		Tree tree = new Tree();
 		currentLowerBound = tree.initSolution();
-		tree.search();
+		search();
 	}
 	
 	/**
-	 * This method searches for the possible viable solution by finding 
+	 * This class searches for the possible viable solution by finding 
 	 * the valid child node with minimum lower bound. 
 	 * @author Ga Hyung Kim
 	 * 
@@ -39,7 +39,7 @@ public class Tree {
 			
 			// If there are no children for the tempNode, close the current
 			// node and move back to the parent node and find another child
-			// Else calcuate the lower bounds of children node and set
+			// Else calculate the lower bounds of children node and set
 			// next node as child node with minimum lower bound
 			if (tempNode.getChildren().size() == 0) {
 				tempNode.setOpen(false);
@@ -61,87 +61,83 @@ public class Tree {
 		return tempNode.getLowerBound();
 	}
 	
-	/**
-	 * The search() function searches for the combination of tasks that has the lowest sum of the penalties.
-	 * @author Esther Kim, Esther Chung
-	 */
-	public void search() {
-		// first create children nodes for the root node if the root node does not have any children.
-		if (!rootNode.getHasChildren()) {
-			createChildren(rootNode);
-		}
-		
-		// Now get the initial list of tasks that are worthwhile to go down;
-		// these are the ones that have smaller LB than the current LB.
+	//check if open
+	// 	if closed go to parent
+	// create children --> check if has children already
+	public static void search() {
 		ArrayList<Node> openChildrenNodes = new ArrayList<Node>();
 		for (Node childNode : rootNode.getChildren()) {
-			calcLowerBound(childNode);
 			if ((childNode.getLowerBound() < currentLowerBound) && childNode.getOpen()) {
 				openChildrenNodes.add(childNode);
 			}
-			else {
-				childNode.setOpen(false);
-			}
 		}
-		
-		// Now traverse around the tree
-		// start off with the node that has the lowest LB
-		// out of the list of the nodes that are worthwhile to go down.
-		Node tempNode = minLowerBound(openChildrenNodes);
+		Node tempNode;
 		Node minChild;
-		while (!openChildrenNodes.isEmpty()) {
-			// create children for the current node (tempNode)
-			// first check if tempNode already has the children created
+		while (openChildrenNodes.size() != 0) {
+			tempNode = minLowerBound(openChildrenNodes);
+			// create children for node
 			if (!tempNode.getHasChildren()) {
-				// if there is no children, then create children 
 				createChildren(tempNode);
-			}
-				
-			// check if children have been created; if they haven't been created
-			// it means that there are no available children for the current node.
-			if (!tempNode.getHasChildren()) {
-				// if there is no child that is available
-				// close this node and remove this node from the openChildren array.
-				// Then go to some other node that is in the openChildrenNodes array
-				// which has the minimum lower bound.
-				// tempNode.setOpen(false);
-				openChildrenNodes.remove(openChildrenNodes.indexOf(tempNode));
-				tempNode = minLowerBound(openChildrenNodes);
-			}
-			else {
-				// if there are children that are available,
-				// add these nodes to the openChildrenNodes array if they have
-				// lower bounds that are smaller than the currentLowerBound.
 				for (Node childNode : tempNode.getChildren()) {
 					calcLowerBound(childNode);
-					if ((childNode.getLowerBound() < currentLowerBound) && childNode.getOpen()) {
-						// add the worthwhile children to the openChildrenNodes
-						// only if we are not at the last machine.
-						if (tempNode.getMachine() != 6) {
-							openChildrenNodes.add(childNode);
-						}
-					}
 				}
-				
-				// if we are at the last node, select the child node that has the
-				// smallest lower bound. Compare that lower bound with the current
-				// lower bound. If it is smaller than the current lower bound, 
-				// update the current lower bound, and update the final solution.
-				if (tempNode.getMachine() == 6) {
-					minChild = minLowerBound(tempNode.getChildren());
-					
-					if (currentLowerBound > minChild.getLowerBound()) {
-						currentLowerBound = minChild.getLowerBound();
-						finalSol = minChild.getHistory();
-					}
-				}
-				
-				// remove the current tempNode and move to the best node
-				openChildrenNodes.remove(openChildrenNodes.indexOf(tempNode));
-				tempNode = minLowerBound(openChildrenNodes);
 			}
+			// when at the end of tree 
+			if (tempNode.getMachine() == 6) {
+				minChild = minLowerBound(tempNode.getChildren());
+				if (currentLowerBound > minChild.getLowerBound()) {
+					currentLowerBound = minChild.getLowerBound();
+					finalSol = minChild.getHistory();
+				}
+				tempNode.setOpen(false);
+				openChildrenNodes.remove(openChildrenNodes.indexOf(tempNode));
+				for (Node childNode : tempNode.getChildren()) {
+					if (currentLowerBound < childNode.getLowerBound()) {
+						openChildrenNodes.remove(openChildrenNodes.indexOf(childNode));	
+					}
+				}
+			}
+			// when at middle of tree
+			else {
+				for (Node childNode : tempNode.getChildren()) {
+					if (currentLowerBound > childNode.getLowerBound()) {
+						openChildrenNodes.add(childNode);	
+					}
+					else {
+						childNode.setOpen(false);
+					}
+				}
+			}
+			
+			
+			
+			//Check if node has children, if not, generate them and calc LB
+			//if current node is node 6, then find best child, find LB for each child
+			//update CLB if possible, and close and remove all nodes in openChildrenNodes 
+			//with LB > CLB
+			//close parent
+			
+			//else:
+			//add all new children with LB < CLB to openChildrenNode
+			
+			
 		}
 	}
+	
+	/* 	
+	 * Node[] openChildrenNodes;
+	 * 
+	 * 		if less than curentLB
+	 * 			check if have children
+	 * 				if no children --> create children
+	 * 					store all children in openChildrenNodes
+	 * 					
+	 * 				else has children 
+	 * 					check if child is closed
+	 * 					store all open children in openChildrenNodes 
+	 * 		else greater than currentLB
+	 * 			close the node 
+	 */
 
 	/**
 	 * Class minLowerBound returns the child node with minimum lower bound
@@ -149,7 +145,7 @@ public class Tree {
 	 * @param children An array of children nodes with same parent
 	 * @return Node[index] Element of an array of children nodes with minimum lower bound
 	 */
- 	public Node minLowerBound(ArrayList<Node> children) {
+ 	public static Node minLowerBound(ArrayList<Node> children) {
 		
 		int min = Integer.MAX_VALUE;
 		int index = -1;
@@ -161,40 +157,15 @@ public class Tree {
 		}
 		return children.get(index);
  	}
- 	
- 	public int adjustLowerBound(Node node) {
- 		int lowerBoundToAdd = 0;
- 		int machineNum = node.getMachine();
- 		
- 		for (int i = machineNum + 1; i < Constraint.penalties.length; i++) {
- 			lowerBoundToAdd = lowerBoundToAdd + getMinValue(i);
- 		}
- 		return lowerBoundToAdd;
- 	}
- 	
- 	// Returns the minimum integer value in given row.
- 	public int getMinValue(int machineNum) {
- 		int min = Integer.MAX_VALUE;
- 		int row = machineNum;
- 		
- 		for (int i = 0; i < Constraint.penalties.length; i++) {
- 			if ((min > Constraint.penalties[row][i]) && (Constraint.penalties[row][i] > -1)) {
- 				min = Constraint.penalties[row][i];
- 			}
- 		}
- 		
- 		return min;
- 	}
 	
  	/**
  	 * This method calculates the total lower bound for every node up to and including the current node and sets the passes in nodes lower bound to the sum
  	 * @param node the node to calculate the lower bound for 
  	 */
-	public void calcLowerBound(Node node) {
+	public static void calcLowerBound(Node node) {
 		Node calcNode = node; //the node whose LB is being calculated
 		int[][] penalty = constraint.getPenalties(); //uses 2D penalty array from the constraint class
 		ArrayList<Character> history = calcNode.getHistory(); //list of the tasks assigned prior to this current node and including this current nodes task
-
 		int lowerbound = calcNode.getLowerBound(); //initialize lowerbound to current nodes set lowerbound (zero or if there is a soft constraint it would take that penalty to start)
 		
 		//calculate the sum of the penalties for the tasks assigned in the given history ArrayList
@@ -215,22 +186,9 @@ public class Tree {
 	 * @author Esther Chung
 	 * @param parent the parent Node from which the children come
 	 */
-	// note: should we calc lowerBound of children in this method or outside (right now doing it outside)
-	public void createChildren(Node parent) {
-		/*
-		 * if not null and not too hard constraint {
-		 *  	create child 
-		 *  	if soft too near constraint
-		 *  		add soft too near penalty to child lowerbound
-		 *  	calcLowerBound 
-		 * } 
-		 * 
-		 */
-		
-		// getting penalty array from constraint class
-		int[][] penalty = constraint.getPenalties();
+	public static void createChildren(Node parent) {
 		// create an array of nodes
-		ArrayList<Node> childrenArray = new ArrayList<Node>();
+		ArrayList<Node> childrenArray = new ArrayList<>();
 		
 		// variables needed
 		int parentMachine = parent.getMachine(); // get the parent's machine #
@@ -241,7 +199,7 @@ public class Tree {
 		for (int i = 0; i < parentMachine; i++) {
 			for (int j = 0; j < availableTasks.length; j++) {
 				if (takenTasks.get(i) == availableTasks[j]) {
-					availableTasks[j] = ' ';
+					availableTasks[j] = Character.MIN_VALUE ;
 					break; 
 				}
 			}
@@ -249,17 +207,8 @@ public class Tree {
 		
 		// initialize the children nodes; create nodes for only the available tasks
 		for (int i = 0; i < availableTasks.length; i++) {
-			if (availableTasks[i] != ' ') { 
-				if (penalty[parentMachine + 1][convertInt(availableTasks[i])] != -1 //checking if penalty spot is null
-				&& !constraint.tooNearH(parent.getTask(), availableTasks[i])) { 		//checking if there is too near hard constraint
-				
-					Node childNode = new Node(parent, parentMachine + 1, availableTasks[i]);
-					int tnsPenalty = constraint.tooNearS(parent.getTask(), availableTasks[i]); //if there is too near soft constraint return penalty if not return 0
-					childNode.setLowerBound(tnsPenalty);
-					int childHistory = parent.getHistory().add(availableTasks[i]);
-					childNode.setHistory(childHistory);
-					childrenArray.add(childNode);
-				}
+			if (availableTasks[i] != Character.MIN_VALUE ) {
+				childrenArray.add(new Node(parent, parentMachine + 1, availableTasks[i]));
 			}
 		}
 		
@@ -268,11 +217,11 @@ public class Tree {
 		parent.setHasChildren(true);
 	}
 
-	public int convertInt(char task){
+	public static int convertInt(char task){
 		return task - 65;
 	}
 
 	public char convertChar(int task){
-		return char(task+65);
+		return (char) (task+65);
 	}
 }
